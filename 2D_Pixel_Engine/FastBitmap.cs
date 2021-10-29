628e86d5789d90c24abea5ae0947586845eac1ab
Will Burland <williamburland10@gmail.com>
2021-10-29 11:39:35 +0100

    Initial commit

---
+++ 2D_Pixel_Engine/FastBitmap.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace _2D_Pixel_Engine
+{
+    public class FastBitmap
+    {
+        private readonly Bitmap contents;
+        private readonly int[] data;
+        private readonly int width;
+        private readonly int height;
+
+        protected GCHandle dataHandle;
+
+        public FastBitmap(int newWidth, int newHeight)
+        {
+            width = newWidth;
+            height = newHeight;
+            data = new int[width * height];
+
+            dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            contents = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, dataHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, Color newColour)
+        {
+            data[x + (y * width)] = newColour.ToArgb();
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            return Color.FromArgb(data[x + (y * width)]);
+        }
+
+        public Bitmap GetContents()
+        {
+            return contents;
+        }
+    }
+}
---
+++ 2D_Pixel_Engine/Window.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace _2D_Pixel_Engine
+{
+    public partial class Window : Form
+    {
+        private static int width = 800;
+        private static int height = 300;
+
+        public Window()
+        {
+            InitializeComponent();
+
+            FastBitmap display = new FastBitmap(width, height);
+            outputPictureBox.Width = width;
+            outputPictureBox.Height = height;
+
+            this.Width = width;
+            this.Width = width + (width - ClientRectangle.Width);
+            this.Height = height;
+            this.Height = height + (height - ClientRectangle.Height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    display.SetPixel(i, j, Color.FromArgb(i % 255, j % 255, 0));
+                }
+            }
+
+
+            outputPictureBox.Image = display.GetContents();
+        }
+    }
+}
